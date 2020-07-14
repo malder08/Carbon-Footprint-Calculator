cf14b7ab45ff46eaa74aa6e570f50ea958ea8ae5
malder08 <39073505+malder08@users.noreply.github.com>
2020-07-14 08:46:14 -0600

    Added Calculation

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -6,111 +6,190 @@ function Questions() {
     // variable and its setter method which will store all our questions and the answer that the user selected
     const [questions, setQuestions] = useState([
         {
-            type: "mc",
             question: "Do you have solar panels?",
 
             // this part of the object is what will store which option the user has selected
             // we should be able to pass these selections into a function that performs the calculation
             currentOption: "none",
-
             options: [
-            "Yes",
-            "No"
-            ]        
-        },
-        {
-            type: "mc",
-            question: "If Not, How Much Money Do You Spend On Electricity Each Month?",
-            currentOption: "none",
-            options: [
-            "$0-$75",
-            "$75-$150",
-            "$150-$250",
-            "$250+",
-            "$111 US Average"
-            ]
-        },
-        {
-            type: "mc",
-            question: "How Many People Live In Your House?",
-            currentOption: "none",
-            options: [
-            "1-2",
-            "3-4",
-            "5+"
-            ]
-        },
-        {
-            type: "mc",
-            question: "In total, how many miles do you drive in a week, or ride in a car?",
-            currentOption: "none",
-            options: [
-            "0-100 -uses public transportation or bikes, only errands otherwise",
-            "100-300 -commutes to work daily, follows a normal routine",
-            "300+ -Uber/Taxi driver, drives a lot",
-            "250 US Average"
-            ]
-        },
-        {
-        type: "mc",
-            question: "What Is Your Car's Average Mileage or MPG?",
-            currentOption: "none",
-            options: [
-            "10-20",
-            "20-30",
-            "30+",
-            ]
-        },
-        {
-        type: "mc",
-            question: "Do You Recycle?",
-            currentOption: "none",
-            options: [
-            "Yes",
-            "No"
-            ]
-        },
-        {
-            type: "mc",
-                question: "Do Run or Use a Compost?",
-                currentOption: "none",
-                options: [
                 "Yes",
                 "No"
-                ]
-            },
-            {
-                type: "mc",
-                    question: "How Many Times a Week Do You Eat Beef?",
-                    currentOption: "none",
-                    options: [
-                    "0",
-                    "1-7",
-                    "8-14",
-                    "15+"
-                    ]
-                },
-                {
-                    type: "mc",
-                        question: "Do You Eat Organic and/or Locally?",
-                        currentOption: "none",
-                        options: [
-                        "Neither",
-                        "Locally",
-                        "Organic",
-                        "Both"
-                        ]
-                    }
+            ],
+            // stores the value that will be added to carbon footprint if the option with the same index is selected
+            effects: [
+                -5000, // this is for "Yes"
+                0 // this is for "No"
+            ],
+            // stores the tip that will be added to the tip array if the option with the same index is selected (not sure how we want to reference tips)
+            qTips: [
+                "none", // this is for "Yes"
+                "Tip #1" // this is for "No"
+            ]
+        },
+        {
+            question: "How much money do you spend on electricity each month?",
+            currentOption: "none",
+            options: [
+                "$0-75",
+                "$75-150",
+                "$150-250",
+                "$250+",
+                "Don't Know"
+            ],
+            effects: [
+                -5000,
+                0,
+                10000,
+                15000,
+                0
+            ],
+            qTips: [
+                "none",
+                "none",
+                "none",
+                "Tip #2",
+                "none"
+            ]
+        },
+        {
+            question: "How many people live in your house?",
+            currentOption: "none",
+            options: [
+                "1-2",
+                "3-4",
+                "5-6",
+                "7+"
+            ],
+            effects: [ //calculated later
+                1.5,
+                3.5,
+                5.5,
+                8
+            ],
+            qTips: [
+                "none",
+                "none",
+                "none",
+                "none",
+            ]
+        },
+        {
+            question: "How many times do you have a meal with beef every week?",
+            currentOption: "none",
+            options: [
+                "0",
+                "1-7",
+                "8-14",
+                "15+",
+            ],
+            effects: [
+                -2000,
+                0,
+                1000,
+                2000
+            ],
+            qTips: [
+                "none",
+                "none",
+                "none",
+                "none",
+            ]
+        },
+        {
+            question: "Do you eat locally and/or organic?",
+            currentOption: "none",
+            options: [
+                "Neither",
+                "Locally",
+                "Organic",
+                "Both",
+            ],
+            effects: [
+                0,
+                0,
+                0,
+                0
+            ],
+            qTips: [
+                "none",
+                "none",
+                "none",
+                "none",
+            ]
+        }
     ])
+
+    // variable to store footprint value
+    const [footprint, setFootprint] = useState(32000);
+
+    // array to store tips
+    const [tips, setTips] = useState([]);
 
     // changes which option is currently selected using the state setter method
     const changeOption = (index, option) => e => {
         let newArr = [...questions]; // copies current array
         newArr[index].currentOption = option; // performs necessary change to copy of array
-
+    
         setQuestions(newArr); // overwrites old array with new, modified version
     }
 
+    // function for the calculation
+    const calculate = () => e => {
+
+        // have to set temporary values that footprint and tips can be set to at the end of the logic
+        var tempFootprint = 32000;
+        var tempTips = [];
+        var solarPanel = false;
+        var electricityBill = 0;
+
+        for (var i = 0; i < questions.length; i++) {
+            
+            // store the current answer you're checking within the loop
+            var answer = questions[i].currentOption;
+            // store the options, effects on footprint, tips for that question
+            var options = questions[i].options;
+            var effects = questions[i].effects;
+            var qTips = questions[i].qTips;
+
+            // go through all of the options for that question
+            for (var j = 0; j < options.length; j++) {
+                // check if the option they selected is equal to the current option in the loop
+                if (answer.localeCompare(options[j]) === 0) {
+
+                    // if it is, add the corresponding effect (of the same index) to the footprint
+                    if (i=== 0 && options[j] === "Yes") { // save the fact that the user has solar panels
+                        solarPanel = true;
+                    }
+                    //run through special cases, last else statement is for normal questions
+                    if (i === 1) { //electricity bill question
+                        if (!solarPanel) {
+                            electricityBill = effects[j]; //save value to be calculated in next question
+                        }
+                    } else if (i === 2 ) { //number of people question
+                        if (!solarPanel) { // only change if user doesn't have solar panels
+                            electricityBill += 5000;
+                            electricityBill /= effects[j];
+                            electricityBill -= 5000;
+                            tempFootprint += electricityBill;
+                        } //else change nothing, effect changed in question 1
+                    } else { //regular question, change normally
+                        tempFootprint += effects[j];
+                    }
+                    
+
+                    // do the same for the corresponding tip
+                    if (qTips[j].localeCompare("none") !== 0) {
+                        tempTips.push(qTips[j]);
+                    }
+                }
+            }
+        }
+
+        // set the state footprint and tip variables to the temporary ones
+        setFootprint(tempFootprint);
+        setTips(tempTips);
+    }
+    
     // this is what will be displayed
     return  (
         <div>
